2170f79a14147e3dbe6918c62d58118c7e021609
Achyut <aoogleak@gmail.com>
2022-11-01 14:33:48 +0545

    1.0 Base React App Created

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <div>
+        <h2>This is App body</h2>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
