f460e3d571037550e607ad3630742ffbf97f9d7a
Achyut <aoogleak@gmail.com>
2022-11-04 12:59:28 +0545

    1.3 Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(incomingNote) {
+    setNotes((prevNotes) => {
+      return [incomingNote, ...prevNotes];
+    });
+  }
+
+  function deleteRequest(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Heading />
-      {notes.map((item) => (
-        <Note key={item.key} title={item.title} content={item.content} />
-      ))}
+      <CreateArea passNote={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            title={note.title}
+            content={note.content}
+            key={index}
+            id={index}
+            onDelete={deleteRequest}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+  }
+  function sendNote(event) {
+    event.preventDefault();
+    props.passNote(note);
+    setNote({ title: "", content: "" });
+  }
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={sendNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
